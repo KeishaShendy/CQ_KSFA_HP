25b732a0298a833529637d9e60ee57d36b0ba874
keishashendy <shendyarrafli7@gmail.com>
2026-03-31 21:43:52 +0700

    Menambahkan latihan 2 yaitu fitur update mata kuliah dengan ExecuteNonQuery

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -119,3 +119,24 @@ namespace PraktikumADO
                 MessageBox.Show("Error hitung dosen: " + ex.Message);
             }
         }
+
+        private void btnUpdateMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh: " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error update mata kuliah: " + ex.Message);
+            }
+        }
