083a9cc45a529175cf3ec6a1f68156e8bc9ec2f5
keishashendy <shendyarrafli7@gmail.com>
2026-03-31 20:28:54 +0700

    Menambahkan fitur hitung jumlah mahasiswa dengan ExecuteScalar

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -38,4 +38,21 @@ namespace PraktikumADO
 
         private void btnHitungMhs_Click(object sender, EventArgs e)
         {
- 
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error hitung mahasiswa: " + ex.Message);
+            }
+        }
