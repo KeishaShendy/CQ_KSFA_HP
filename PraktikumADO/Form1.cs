5901ebcb405dd89b1ec9f923b055d3b6e7e78294
keishashendy <shendyarrafli7@gmail.com>
2026-03-31 20:24:25 +0700

    Menambahkan logika koneksi database pada btnConnect_Click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -23,4 +23,10 @@ namespace PraktikumADO
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+                conn.Close();
  
